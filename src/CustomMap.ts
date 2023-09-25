3452984a87ef9019b48d3db393924f97d2340fbc
Adem Hoskin <ademjhoskin@gmail.com>
2023-09-25 13:55:40 -0400

    prettier formatting

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,29 +1,27 @@
-import { Mappable } from "./Interface";
+import { Mappable } from './Interface';
 
-export class CustomMap{
-    private googleMap: google.maps.Map;
+export class CustomMap {
+  private googleMap: google.maps.Map;
 
-    constructor(divId: string){
-        this.googleMap = new google.maps.Map(document.getElementById(divId) as HTMLElement, {
-            zoom: 1,
-            center: {
-                lat: 0,
-                lng: 0
-            }
-        })
-    }
-    addMarker(mappable: Mappable): void{
-        const marker = new google.maps.Marker({
-            map: this.googleMap,
-            position: {
-                lat: mappable.location.lat,
-                lng: mappable.location.lng
-                }
-            }
-        )
-    }
-
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(
+      document.getElementById(divId) as HTMLElement,
+      {
+        zoom: 1,
+        center: {
+          lat: 0,
+          lng: 0,
+        },
+      }
+    );
+  }
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
+      },
+    });
+  }
 }
-
-
-
